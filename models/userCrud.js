e1a9170707956d0f6547a4b7254bd7d0b29d1194
sourav <sourav.codknox@gmail.com>
2026-05-14 13:04:31 +0530

    add index to email field in User and userCrud schemas

--- models/userCrud.js
+++ models/userCrud.js
@@ -4,7 +4,8 @@ const userCrudSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        index : true
     },
     password: {
         type: String,
